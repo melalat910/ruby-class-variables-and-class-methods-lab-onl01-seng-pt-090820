1f89f1f2e469d1761540a090f9abea7c5fefa107
Tirzah P. Howard <tp.howard47@gmail.com>
2020-10-01 01:56:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,19 @@
 class Song
   attr_accessor  :name, :artist, :genre
 
+  @@count = 0;  #total songs created
+    @@genres = []; #array of genres
+    @@artists = []; #array of unique artists
+    @@genre_count = {}; #hash of genre and number of songs in genre created
+    @@artist_count = {}; #hash of artists and number of songs created
+
+    def initialize(name, artist, genre)
+        #Setup Class variables
+        @@count = @@count + 1;
+        @@genres << genre;
+        @@artists << artist
+
+        @name = name
+        @artist = artist
+        @genre = genre
 end
